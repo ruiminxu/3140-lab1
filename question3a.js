a774d145d060a07ed200728f27b3b5550003cd45
ruiminxu <64786097+ruiminxu@users.noreply.github.com>
2021-04-14 14:52:49 -0400

    Update question3a.js

--- question3a.js
+++ question3a.js
@@ -1,13 +1,13 @@
-let rectangleImperial = { length: 100, width: 50, height: 70 };
+let rectangleImperial = { length: 100, width: 50, height: 70 };  //create an object for rectangle with Imperial measurement
 
-let rectangleMetric = { length: 100, width: 50, height: 70 };
+let rectangleMetric = { length: 100, width: 50, height: 70 }; //create an object for rectangle with Metric measurement
 
-untiConverter("imperial", rectangleImperial);
+untiConverter("imperial", rectangleImperial); //Call function
 
-untiConverter("metric", rectangleMetric);
+untiConverter("metric", rectangleMetric); //Call function
 
 function untiConverter(unit, object) {
-	if (unit === "metric") {
+	if (unit === "metric") {  //if it is metric, then do the following by dividing 2.54
 		let newRectangle = {
 			length: object.length / 2.54,
 			width: object.width / 2.54,
@@ -15,7 +15,7 @@ function untiConverter(unit, object) {
 		};
 
 		return newRectangle;
-	} else if (unit === "imperial") {
+	} else if (unit === "imperial") {  //if it is imperial, then do the following by mulitplie 2.54
 		let newRectangle = {
 			length: object.length * 2.54,
 			width: object.width * 2.54,
